0235c0348f5978758d0599703a2bf5505b8e4ed5
Felipe Limeira <limeira.felipe94@gmail.com>
2023-12-01 16:07:18 -0300

    fix: error subscriptions

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,2 +1,8 @@
+from django.shortcuts import render
+from eventex.subscriptions.forms import SubscriptionForm
+
+
 def subscribe(request):
-    pass
+    context = {'form': SubscriptionForm()}
+    return render(request, 'subscriptions/subscription_form.html', context)
+
